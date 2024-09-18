63cad8861806923f02cd022fe0296840aee2e265
Patrick Wilson <patrickafriyiewilson12345@gmail.com>
2024-09-18 18:32:29 +0000

    herosection

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="hero-section">
     <header class="header">
-      <div class="header-left">ALVIN & Associate</div>
+      <div class="header-left">
+        <span class="alvin">ALVIN</span> & <span class="asso">Associate</span>
+      </div>
       <div class="header-center">
         <ul class="nav-list">
           <li>Home</li>
@@ -10,10 +12,10 @@
           <li>Practice Area</li>
           <li>Testimonials</li>
           <li>FAQs</li>
-          <li>Contact Us</li>
+          <button class="contact"><li>Contact Us</li></button>
         </ul>
       </div>
-      <div class="header-right">+233257732425</div>
+      <div class="header-right">+233-257-732-425</div>
     </header>
     <div class="hero-content">
       <div class="hero-text">
@@ -35,9 +37,9 @@ export default {
 
 <style scoped>
 .hero-section {
-  background-color: Blue;
+  background-color: #002a7d;
   color: white;
-  padding: 20px;
+  background-image: url(/src/assets/);
 }
 
 .header {
@@ -49,13 +51,49 @@ export default {
   color: black;
 }
 
+.alvin {
+  color: #e63946;
+  width: 116px;
+  height: 43px;
+  font-family: 'Sora' sans-serif;
+  font-size: 34px;
+  font-weight: 800;
+  line-height: 42.84px;
+  letter-spacing: 0.04em;
+  text-align: left;
+}
+
 .header-left {
   font-size: 1.2rem;
   font-weight: bold;
+  width: 190px;
+}
+
+.asso {
+  width: 116px;
+  height: 18px;
+  gap: 0px;
+  opacity: 0px;
+  font-family: 'Plus Jakarta Sans' sans-serif;
+  font-size: 14px;
+  font-weight: 700;
+  line-height: 17.64px;
+  letter-spacing: 0.08em;
+  text-align: left;
 }
 
 .header-center {
   text-align: center;
+}
+
+.contact {
+  color: white;
+  background-color: red;
+  padding-left: 20px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-right: 20px;
+  border: none;
 }
 
 .nav-list {
@@ -64,6 +102,11 @@ export default {
   margin: 0;
   display: flex;
   gap: 15px;
+  height: 32px;
+  font-family: 'Roboto' sans-serif;
+  font-size: 16px;
+  font-weight: 600;
+  color: #2e294e;
 }
 
 .nav-list li {
@@ -72,6 +115,7 @@ export default {
 
 .header-right {
   font-size: 1rem;
+  color: red;
 }
 
 .hero-content {
@@ -86,15 +130,24 @@ export default {
 }
 
 .hero-text h1 {
-  font-size: 2rem;
   margin-bottom: 20px;
+  margin-left: 180px;
+  width: 896px;
+  height: 348px;
+  font-family: 'Plus Jakarta Sans' sans-serif;
+  font-size: 92px;
+  font-weight: 800;
+  line-height: 115.92px;
+  text-align: left;
+  color: #ffffff;
 }
 
 .consultation-button {
   background-color: red;
   color: white;
-  padding: 10px 20px;
+  padding: 20px 30px;
   border: none;
+  margin-left: 180px;
   border-radius: 5px;
   font-size: 1rem;
   cursor: pointer;
